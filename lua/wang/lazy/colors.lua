f8ff91af85b16620e9a1c7e77d7ef169d4610c27
wang <wangminhong73@gmail.com>
2024-02-27 11:05:26 +0800

    add lsp from repo

--- lua/wang/lazy/colors.lua
+++ lua/wang/lazy/colors.lua
@@ -1,20 +1,26 @@
-function ColorMyPencils(color)
-	color = "nordfox"
-	vim.cmd.colorscheme(color)
-
-end
-
 return {
     {
         "EdenEast/nightfox.nvim",
         config = function()
             require('nightfox').setup({
-                disable_background = true,
+                options = {
+                    transparent  = true,
+                    terminal_colors = true,
+                }
             })
 
             vim.cmd("colorscheme nordfox")
 
-            ColorMyPencils()
         end
     },
+    {
+        "olivercederborg/poimandres.nvim",
+        config = function()
+            require('poimandres').setup {
+                disable_background = true,
+                disable_float_background = true,
+            }
+            --vim.cmd("colorscheme poimandres")
+        end
+    }
 }
